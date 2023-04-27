a58866853cf1592a66681b09178abff5f646cbc3
Reddy <moole-srinivasa.reddy@capgemini.com>
2023-04-27 14:11:36 +0530

    terrfaorm demo

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+
+access_key = "AKIAVTKSYXMB7BVD62U2"
+secret_key = "36QmpubBulWVjVMiVKhqisjEr+SxVQKNQi22M2jF"
+region = "ap-southeast-1"
+}
+
+
+resource "aws_security_group" "example" {
+  name = "example"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example-security-group"
+  }
+}
+
+resource "aws_instance" "instance1" {
+ami = "ami-05930ce55ebfd2930"
+instance_type = "t2.micro"
+#security_groups = ["example"]
+vpc_security_group_ids= [aws_security_group.example.id]
+}
